afd9fca7b912f32a3674a7164e89f67ff57c7f56
Myriam <my.greder@gmail.com>
2021-07-17 20:30:11 +0200

    first commit

---
+++ src/js/datalogic.js
@@ -0,0 +1,145 @@
+import data from "./data";
+import state from "./state";
+
+import { normalizeText } from "./utils";
+
+/**
+ Chercher et Obtenir une liste de tous les ingrédients de toutes les recettes
+  @return  {array}  : La liste des ingrédients.*/
+const getAllIngredients = () => {
+  let ingredients = [];
+  data.recipes.forEach((recipe) => {
+    recipe.ingredients.forEach((ing) => {
+      if (!ingredients.includes(ing.ingredient.toLowerCase()))
+        ingredients.push(ing.ingredient.toLowerCase());
+    });
+  });
+  return ingredients.map((ing) => ({
+    type: "ing",
+    name: ing,
+  }));
+};
+
+/*Obtenez une liste de tous les appareils ménagers de toutes les recettes
+  @return {array} la liste des appareils*/
+const getAllAppliances = () => {
+  let appliances = [];
+  data.recipes.forEach((recipe) => {
+    if (!appliances.includes(recipe.appliance.toLowerCase()))
+      appliances.push(recipe.appliance.toLowerCase());
+  });
+  return appliances.map((app) => ({
+    type: "app",
+    name: app,
+  }));
+};
+
+/*Obtenir une liste de tous les ustensiles de cuisine de toutes les recettes
+ @return {array} : la liste sous forme de tableau*/
+const getAllUstensils = () => {
+  let ustensils = [];
+  data.recipes.forEach((recipe) => {
+    recipe.ustensils.forEach((ustensil) => {
+      if (!ustensils.includes(ustensil.toLowerCase()))
+        ustensils.push(ustensil.toLowerCase());
+    });
+  });
+  return ustensils.map((ust) => ({
+    type: "ust",
+    name: ust,
+  }));
+};
+
+/*Obtenir une liste de tous les identifiants de recettes
+ @return {array} : la liste des identifiants.*/
+const getAllRecipeIds = () => {
+  return data.recipes.map((elt) => elt.id);
+};
+
+/*Initialiser l'état avec les données initiales
+  @returns : {void}*/
+const initializeState = () => {
+  state.displayedIng = getAllIngredients();
+  state.displayedApp = getAllAppliances();
+  state.displayedUst = getAllUstensils();
+  state.displayedRecipes = getAllRecipeIds();
+  state.ingObj = getIngredientsObject();
+  state.appObj = getAppliancesObject();
+  state.ustObj = getUstensilsObject();
+};
+
+/*Obtenir une seule chaîne avec tous les ingrédients d'une recette
+ @param   {object}  : recette.
+ @returns {string} : chaine d'ingrédients*/
+const getIngredientsStringFromRecipe = (recipe) => {
+  let ingredientsString = "";
+  recipe.ingredients.forEach((ing) => {
+    ingredientsString += ` ${normalizeText(ing.ingredient)}`;
+  });
+  return ingredientsString;
+};
+
+/*Obtenir une liste de tous les ingrédients(objets) de toutes les recettes sous forme de tableau
+  @param   {array}  tagListe : le tableau des filtres.
+  @return  {object} : objet*/
+const createTagObject = (tagList) => {
+  let tagObj = {};
+  tagList.forEach((tag) => (tagObj[normalizeText(tag.name)] = []));
+
+  return tagObj;
+};
+
+/*Obtenir un objet avec tous les noms d'ingrédients
+ @return  {object} : objet*/
+const getIngredientsObject = () => {
+  let ingredientsObject = createTagObject(getAllIngredients());
+  data.recipes.forEach((recipe) => {
+    recipe.ingredients.forEach((elt) => {
+      const objKey = normalizeText(elt.ingredient);
+      ingredientsObject[objKey].push(recipe.id);
+    });
+  });
+  return ingredientsObject;
+};
+
+/*Obtenir un objet avec tous les noms d'appareils 
+  @return  {object} : objet*/
+const getAppliancesObject = () => {
+  let appliancesObject = createTagObject(getAllAppliances());
+
+  data.recipes.forEach((recipe) => {
+    const objKey = normalizeText(recipe.appliance);
+    appliancesObject[objKey].push(recipe.id);
+  });
+
+  return appliancesObject;
+};
+
+/* Obtenir un objet avec tous les noms d'ustensiles
+ @return  {object}: objet*/
+const getUstensilsObject = () => {
+  let ustensilsObject = createTagObject(getAllUstensils());
+
+  data.recipes.forEach((recipe) => {
+    recipe.ustensils.forEach((elt) => {
+      const objKey = normalizeText(elt);
+      ustensilsObject[objKey].push(recipe.id);
+    });
+  });
+
+  return ustensilsObject;
+};
+
+/*
+ Obtenir les données de recettes complètes à partir des identifiants
+ @return  {array}: tableau*/
+const getFullRecipesFromIds = (idsArray) => {
+  return data.recipes.filter((recipe) => idsArray.includes(recipe.id));
+};
+
+export {
+  initializeState,
+  getIngredientsStringFromRecipe,
+  getFullRecipesFromIds,
+  getAllRecipeIds,
+};
---
+++ src/js/recipes.js
@@ -0,0 +1,93 @@
+import data from "./data";
+import { createGenericElt, createLinkElt } from "./utils";
+
+//DOM Variables//
+const mainContentElt = document.getElementById("main-content");
+
+/*Créer un modèle de recette unique
+ @param   {object}  recipe
+ @return  {node} : l'élément créé*/
+const createRecipeElement = (recipe) => {
+  const elt = createGenericElt("article", "recipe");
+  elt.id = recipe.id;
+  const aElt = createLinkElt("index.html", recipe.name);
+  aElt.textContent = "";
+  const imgElt = createGenericElt("div", "recipe-img");
+  const dataElt = createGenericElt("div", "recipe-data");
+  const headerElt = createGenericElt("header");
+  const h2Elt = createGenericElt("h2");
+  const timeElt = createGenericElt("div", "recipe-time");
+  const categoryLeftElt = createGenericElt("div", "recipe-category");
+  const categoryRightElt = createGenericElt("div", "recipe-category");
+
+  const ulElt = createGenericElt("ul");
+  recipe.ingredients.forEach((ingredient) => {
+    const liElt = createIngredient(ingredient);
+    ulElt.appendChild(liElt);
+  });
+
+  h2Elt.textContent = recipe.name;
+  timeElt.innerHTML = `<span class="far fa-clock"></span> ${recipe.time} min`;
+  categoryRightElt.textContent = recipe.description;
+
+  headerElt.appendChild(h2Elt);
+  headerElt.appendChild(timeElt);
+  categoryLeftElt.appendChild(ulElt);
+  dataElt.appendChild(headerElt);
+  dataElt.appendChild(categoryLeftElt);
+  dataElt.appendChild(categoryRightElt);
+  aElt.appendChild(imgElt);
+  aElt.appendChild(dataElt);
+  elt.appendChild(aElt);
+  elt.style.display = "block";
+
+  return elt;
+};
+
+/*Créer un seul ingrédient
+ @param   {object}  ingredient
+ @return  {node}    the created element*/
+const createIngredient = (ingredient) => {
+  const liElt = document.createElement("li");
+  liElt.innerHTML = `<strong>${ingredient.ingredient}`;
+  liElt.innerHTML += ingredient.quantity
+    ? ` : </strong><span>${ingredient.quantity}`
+    : `</strong>`;
+  liElt.innerHTML += ingredient.unit ? ` ${ingredient.unit}</span>` : `</span>`;
+
+  return liElt;
+};
+
+/*Créez toutes les recettes et insérez-les dans le DOM
+ @returns {void}*/
+const createAllRecipes = () => {
+  mainContentElt.innerHTML = "";
+  data.recipes.forEach((recipe) => {
+    const recipeElt = createRecipeElement(recipe);
+    recipeElt.style.display = "block";
+    mainContentElt.appendChild(recipeElt);
+  });
+};
+
+/*Afficher toutes les recettes dans le DOM
+  @returns {void}*/
+const displayAllRecipes = () => {
+  data.recipes.forEach((recipe) => {
+    const recipeToDisplay = document.getElementById(recipe.id);
+    recipeToDisplay.style.display = "block";
+  });
+};
+
+/*Vérifiez si la recette est déjà dans le DOM et supprimez-la
+ @returns {void}*/
+const removeRecipeById = (recipeId) => {
+  if (document.getElementById(recipeId))
+    document.getElementById(recipeId).remove();
+};
+
+export {
+  displayAllRecipes,
+  createRecipeElement,
+  removeRecipeById,
+  createAllRecipes,
+};
